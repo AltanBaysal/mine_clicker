dbcdb46da1cdce1de3c460eb1756c97eb9660848
AltanBaysal <96086932+AltanBaysal@users.noreply.github.com>
2023-01-18 16:11:15 +0300

    Money Indicator Design Improved
    Money Indicator Design Improved

--- lib/feature/game/view/widgets/money_indicator.dart
+++ lib/feature/game/view/widgets/money_indicator.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mine_clicker/core/constants/app_image_paths.dart';
+import 'package:mine_clicker/core/constants/theme/app_text_styles.dart';
+import 'package:mine_clicker/core/utils/screen_size.dart';
+
+import '../../../../core/init/injection_container.dart';
 
 class MoneyIndicator extends StatelessWidget {
   const MoneyIndicator({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Align(
-      alignment: Alignment.topRight,
-      child: Text("assdasd"),
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SizedBox(
+          height: sl<ScreenSize>().getWidthPercent(.1),
+          width: sl<ScreenSize>().getWidthPercent(.1),
+          child: SvgPicture.asset(
+            AppImages.coalIngot,
+            fit: BoxFit.contain,
+          ),
+        ),
+        Text(
+          "99999",
+          style: AppTextStyles.body16SemiBold,
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+      ],
     );
   }
 }
