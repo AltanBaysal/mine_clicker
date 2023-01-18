81769bedf268e3edcd3b38b8973bed18039a8658
AltanBaysal <96086932+AltanBaysal@users.noreply.github.com>
2023-01-18 16:14:03 +0300

    BlockWidget Made More Responsive

--- lib/feature/game/view/widgets/block_widget.dart
+++ lib/feature/game/view/widgets/block_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import '../../../../core/constants/app_image_paths.dart';
 import '../../../../core/init/injection_container.dart';
+import '../../../../core/utils/screen_size.dart';
 import '../../view_model/game_provider.dart';
 
 class BlockWidget extends StatelessWidget {
@@ -12,8 +13,8 @@ class BlockWidget extends StatelessWidget {
     return GestureDetector(
       onTap: sl<GameProvider>().clickBlock,
       child: SizedBox(
-        height: 200,
-        width: 200,
+        height: sl<ScreenSize>().getWidthPercent(.4),
+        width: sl<ScreenSize>().getWidthPercent(.4),
         child: SvgPicture.asset(
           AppImages.coalBlock,
           fit: BoxFit.fitHeight,
